1f92735ef4c4e021446d0a1e898cfd533ede5040
Jamal Abdul-Majid <jamalmajid@Jamals-MBP.attlocal.net>
2023-06-06 17:13:10 -0400

    updated header

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -4,29 +4,27 @@ import Navbar from "./Navbar";
 import Brackets from "../Resources/images/Brackets.svg";
 import { RightArrow } from "../Resources/Svgs";
 import { ButtonPrimary } from "../utils/buttons";
-
+// fix header item alignment
+// fix header hight
 const Header = () => {
   const Heading = () => {
     return (
-      <section id="Header" className="relative top-[210px] left-0">
-        <div className="w-3/6">
-          <h1 className="text-white font-medium not-italic text-2xl lg:text-8xl">
+      <section id="Header" className="flex justify-start">
+        <div className="">
+          <h1 className="text-white font-medium not-italic text-4xl lg:text-8xl">
             Eat, sleep
             <br />
-            <span className="text-majidTech">&lt;code&gt;</span>, repeat
+            <span className="text-majidTech">&lt;code&gt;</span>
+            <span>, repeat</span>
           </h1>
-          <p className="text-silverDard break-keep font-modernEra mt-7 text-xs lg:text-xl">
+          <p className="text-silverDard break-keep font-modernEra md:my-4 lg:my-7 text-xs lg:text-xl">
             I'm a creative developer with years of experience in building products and appealing web
             experiences. I've collaborated with individuals and teams to build experiences for SMEs
             and large enterprises including Wise, Google, Interswitch and Intelia.
           </p>
+          <ButtonPrimary Arrow={RightArrow} text="Start a Project" customClass="text-xl" />
         </div>
-        <img
-          alt="Brackets"
-          className="absolute right-0 top-[-5rem] h-[10%] lg:h-[50%] bg-no-repeat"
-          src={Brackets}
-        ></img>
-        <ButtonPrimary Arrow={RightArrow} text="Start a Project" customClass="text-xl" />
+        <img alt="Brackets" className="h-[50%] bg-no-repeat" src={Brackets}></img>
       </section>
     );
   };
