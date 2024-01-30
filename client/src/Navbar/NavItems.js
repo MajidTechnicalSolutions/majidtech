e9dab9f9ac8df019ba92183797a215a29b6a2ce4
jamalabd <jamalabdul47@gmail.com>
2024-01-30 13:23:33 -0500

    fix: mobile nav li outline

---
+++ client/src/Navbar/NavItems.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { UpArrow } from "../Resources/Svgs";
+import { ButtonPrimary } from "../utils/buttons";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { Link } from "react-scroll";
+import MobileNav from "./MobileNav";
+
+const NavItems = () => {
+  const [listItems, setListItems] = useState({
+    activeObject: 0,
+    objects: [
+      { title: "Home", id: 0 },
+      { title: "Selectedwork", id: 1 },
+      { title: "Services", id: 2 },
+      { title: "Testimonials", id: 3 },
+      { title: "Blog", id: 4 },
+      { title: `Contact`, id: 5, component: UpArrow },
+    ],
+  });
+  return <div>NavItems</div>;
+};
+
+export default NavItems;
