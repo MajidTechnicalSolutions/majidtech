847157751109e9624e8f92a80f300a58f66ce1aa
jamalabd <jamalabdul47@gmail.com>
2024-02-18 17:26:18 -0500

    Fix: to many tags on closed blog.

--- client/src/utils/Tags/Tags.js
+++ client/src/utils/Tags/Tags.js
@@ -3,7 +3,7 @@ import React from "react";
 const Tags = ({ tagNames, tagStyle }) => {
   return (
     <div className="flex">
-      {tagNames.map((tagName) => (
+      {tagNames.toSpliced(2).map((tagName) => (
         <span className={`bg-tagDark text-majidTech rounded p-1.5 justify-between ${tagStyle}`}>
           {tagName}
         </span>
